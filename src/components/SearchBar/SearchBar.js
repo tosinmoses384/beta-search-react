e479b87c083f695679d9dfdb3ec90c8ea01f0c67
Israel <tosinmoses384@gmail.com>
2019-07-26 18:39:32 +0100

    added searchbar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -14,7 +14,7 @@ const sortByOptions = {
     renderSortByOptions() {
         return Object.keys(sortByOptions).map(sortByOption => {
             let sortByOptionValue = sortByOptions[sortByOption];
-            return <li key='sortByOptionValue'>{sortByOptionValue[sortByOption]}</li>;
+            return <li key={sortByOptionValue}>{sortByOption}</li>;
         });
       }
 
