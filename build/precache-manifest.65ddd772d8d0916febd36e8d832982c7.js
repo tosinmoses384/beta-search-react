1f84ec6abb837b20816c0d07832c4c569e21acc2
Tosin <tosinmoses384@gmail.com>
2019-07-27 16:04:02 +0100

    added build

---
+++ build/precache-manifest.65ddd772d8d0916febd36e8d832982c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "332fd2643a4c91516b818aded45fcce0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae1b907edc434b78e26a",
+    "url": "/static/css/main.e3a49c38.chunk.css"
+  },
+  {
+    "revision": "d2d82d68c7df91bb776f",
+    "url": "/static/js/2.e703e8cc.chunk.js"
+  },
+  {
+    "revision": "ae1b907edc434b78e26a",
+    "url": "/static/js/main.77dcc4de.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "74e6a8b0e0bc4bfe90f9a14080b7fdcd",
+    "url": "/static/media/background_search_desktop.74e6a8b0.jpg"
+  },
+  {
+    "revision": "eeac88f134fedee4150544a5f37a039a",
+    "url": "/static/media/background_search_mobile.eeac88f1.jpg"
+  }
+]);
