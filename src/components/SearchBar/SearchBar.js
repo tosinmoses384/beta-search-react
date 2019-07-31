030cb139e22cd91da51dcfe7eb0841125ebbc7df
Tosin <tosinmoses384@gmail.com>
2019-07-31 13:17:27 +0100

    setting the state of components

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,20 +1,55 @@
 import React from 'react';
 import './SearchBar.css';
 
-
-
 const sortByOptions = {
-    'Best Match': 'best_match',
-    'Highest Rated': 'rating',
-    'Most Reviewed': 'review_count'
-  }
-
+  'Best Match': 'best_match',
+  'Highest Rated': 'rating',
+  'Most Reviewed': 'review_count'
+}
 
   class SearchBar extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        term: '',
+        location: '',
+        sortBy: 'best_match'
+      };
+
+      this.handleTermChange = this.handleTermChange.bind(this);
+      this.handleLocationChange = this.handleLocationChange.bind(this);
+      this.handleSearch = this.handleSearch.bind(this);
+      
+    }
+
+    handleSearch(event) {
+      this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+      event.preventDefault();
+    }
+
+    handleTermChange(event) {
+      this.setState({ term: event.target.value });
+    }
+
+    handleLocationChange(event) {
+      this.setState({ location: event.target.value});
+    }
+
+    getSortByClass(sortByOption) {
+      if (sortByOption === this.state.sortBy) {
+         return 'active';
+      }
+      return '';
+    }
+
+    handleSortByChange(sortByOption) {
+      this.setState( {sortBy: sortByOption});
+    }
+
     renderSortByOptions() {
         return Object.keys(sortByOptions).map(sortByOption => {
             let sortByOptionValue = sortByOptions[sortByOption];
-            return <li key={sortByOptionValue}>{sortByOption}</li>;
+            return <li key={sortByOptionValue} onClick={this.handleSortByChange.bind(this, sortByOptionValue)} className={this.getSortByClass(sortByOptionValue)}>{sortByOption}</li>;
         });
       }
 
@@ -25,11 +60,11 @@ const sortByOptions = {
               <ul>{this.renderSortByOptions()}</ul>
             </div>
             <div className="SearchBar-fields">
-              <input placeholder="Search for flight, accommodation and ride" />
-              <input placeholder="Where?" />
+              <input placeholder="Search for businesses anywhere in the world" onChange={this.handleTermChange}/>
+              <input placeholder="Where?" onChange={this.handleLocationChange}/>
             </div>
             <div className="SearchBar-submit">
-              <a href="www.#.com">Let's Go</a>
+              <a onClick={this.handleSearch} href="www.#.com">Let's Go</a>
             </div>
           </div>
         );
