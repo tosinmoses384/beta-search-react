93f7ba011dd64b3221e60dae6cf670ea54e1b78c
Tosin <tosinmoses384@gmail.com>
2019-08-06 04:41:15 +0100

    remove-hard-coded-information

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,24 +3,48 @@ import './App.css';
 // import logo from './logo.svg';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
+import Yelp from '../../util/Yelp';
 
-let business = {
-  imageSrc: 'https://media.api.timbu.com/img/big/1-oak-place-abuja-68370.png',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-};
 
-const businesses = [business, business, business, business, business, business];
+// %%%%%%%%%%%%%%%%%%%%%%%%
+// REMOVE HARD CODED VALUE
+// %%%%%%%%%%%%%%%%%%%%%%%%
+// // let business = {
+//   imageSrc: 'https://media.api.timbu.com/img/big/1-oak-place-abuja-68370.png',
+//   name: 'MarginOtto Pizzeria',
+//   address: '1010 Paddington Way',
+//   city: 'Flavortown',
+//   state: 'NY',
+//   zipCode: '10101',
+//   category: 'Italian',
+//   rating: 4.5,
+//   reviewCount: 90
+// };
+
+// const businesses = [
+//   business, 
+//   business, 
+//   business, 
+//   business, 
+//   business, 
+//   business
+// ];
+
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      businesses: [],
+    },
+    this.searchYelp = this.searchYelp.bind(this);
+  }
+
   searchYelp(term, location, sortBy) {
-    console.log(`You are searching for ${term}, ${location}, and ${sortBy}`);
+    Yelp.searchYelp(term, location, sortBy).then((businesses) => {
+      this.setState({businesses: businesses});
+    });
+    // console.log(`You are searching for ${term}, ${location}, and ${sortBy}`);
   }
 
   render() {
@@ -28,7 +52,7 @@ class App extends React.Component {
       <div className="App">
         <h1>beta-search</h1>
         <SearchBar searchYelp={this.searchYelp} />
-        <BusinessList businesses={businesses} />
+        <BusinessList businesses={this.state.businesses} />
       </div>
     );
   }
